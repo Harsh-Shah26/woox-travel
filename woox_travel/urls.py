1e8ca4714e1a67fa16e9919b19ba4a38040268bf
coder <calmcoder2025@gmail.com>
2026-06-23 17:12:47 +0530

    Serve media files on render

--- woox_travel/urls.py
+++ woox_travel/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from woox_travel import views
 
 
@@ -51,5 +52,6 @@ urlpatterns = [
     path('download-invoice/<int:rid>/', views.download_invoice_pdf, name='download_invoice_pdf'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
