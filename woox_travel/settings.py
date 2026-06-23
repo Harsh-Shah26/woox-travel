5e7a0b064cf5a8c67b36e34f2357f53df4714075
coder <calmcoder2025@gmail.com>
2026-06-23 16:12:39 +0530

    Update settings for deployment

--- woox_travel/settings.py
+++ woox_travel/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@y07l^^r6ufw+o(0glak!&%j%9(g3=0dyzwj!#+nn3!oz8@ulh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
