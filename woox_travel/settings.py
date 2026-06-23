d8dec7e5fdd4566dacf271094e57cb271acc29a2
coder <calmcoder2025@gmail.com>
2026-06-23 16:49:44 +0530

    Fix static files for render

--- woox_travel/settings.py
+++ woox_travel/settings.py
@@ -85,6 +85,7 @@ AUTHENTICATION_BACKENDS=[
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -174,9 +175,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #to import static folder
 
-STATICFILES_DIRS = [
-    BASE_DIR,"static"
-]
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 #to import media folder
 
